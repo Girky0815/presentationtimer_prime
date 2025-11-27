ad7bbd71133aad0441c2fc6ae1bdec4a4ef71c45
稲垣 颯 <rmkb0427@gmail.com>
2025-11-27 19:40:14 +0900

    v1.2.0: ウィンドウのタイトル表示改良

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"presentationtimer_prime", origin, size)) {
+
+  std::string version_str = std::to_string(FLUTTER_VERSION_MAJOR) + "." +
+                            std::to_string(FLUTTER_VERSION_MINOR) + "." +
+                            std::to_string(FLUTTER_VERSION_PATCH);
+  std::wstring title = L"プレゼンタイマー Prime (仮) v" + Utf16FromUtf8(version_str);
+
+  if (!window.Create(title, origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
